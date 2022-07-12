d5a2baa569026c8da25ab141d964f0e6668aedb7
armaansharmaa <armaan.sh03@gmail.com>
2022-07-12 20:10:33 +0530

    Phase-2 ReactUI

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import UserComponent from './components/UserComponent';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <UserComponent />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/UserComponent.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import UserService from '../services/UserService';
+
+class UserComponent extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            users: []
+        }
+    }
+
+    componentDidMount() {
+        UserService.getUsers().then((response) => {
+            this.setState({ users: response.data })
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1 className="text-center"> Users List</h1>
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+
+                            <td>Id</td>
+                            <td>First Name</td>
+                            <td>Last Name</td>
+                            <td>Middle Name</td>
+                            <td>Email</td>
+                            <td>Street</td>
+                            <td>City</td>
+                            <td>State</td>
+                            <td>Country</td>
+                            <td>Pin Code</td>
+                            <td>Employer Name</td>
+                            <td>Employer Id</td>
+                            <td>Employer Street</td>
+                            <td>Employer City</td>
+                            <td>Employer State</td>
+                            <td>Employer Country</td>
+                            <td>Employer Pin Code</td>
+                        </tr>
+
+                    </thead>
+                    <tbody>
+                        {
+                            this.state.users.map(
+                                user =>
+                                    <tr key={user.id}>
+                                        <td> {user.id}</td>
+                                        <td> {user.firstName}</td>
+                                        <td> {user.lastName}</td>
+                                        <td> {user.middleName}</td>
+                                        <td>{user.email}</td>
+                                        <td>{user.street}</td>
+                                        <td>{user.city}</td>
+                                        <td>{user.state}</td>
+                                        <td>{user.country}</td>
+                                        <td>{user.pincode}</td>
+                                        <td>{user.employerName}</td>
+                                        <td>{user.employerId}</td>
+                                        <td>{user.employerStreet}</td>
+                                        <td>{user.employerCity}</td>
+                                        <td>{user.employerState}</td>
+                                        <td>{user.employerCountry}</td>
+                                        <td>{user.employerPincode}</td>
+                                    </tr>
+                            )
+                        }
+
+                    </tbody>
+                </table>
+
+            </div>
+
+        )
+    }
+}
+
+export default UserComponent
